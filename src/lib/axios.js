833ef8ac86d6c2b9d3e5540d209397cdb60b82a1
unixnexo <unixnexo@gmail.com>
2025-08-14 11:38:20 +0330

    f

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://mindmess.runasp.net/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://mindmess.runasp.net/api',
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
